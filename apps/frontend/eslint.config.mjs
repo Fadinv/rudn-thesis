eca8b716bf330c499c3d7fce09368703ec476941
Владимир Фадин <vfadin@24hsoft.com>
2025-04-06 11:31:29 +0300

    frontend. eslint.config.mjs - удалил chakra сниппеты из проверки линта

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
     {
-        ignores: ["src/generated/**"], // ⬅️ вот это добавляем
+        ignores: ["src/generated/**", "src/components/ui/**"], // ⬅️ вот это добавляем
     },
     ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
