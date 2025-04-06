a5d2584e18bf3008117419c87efd78adee1267b0
Владимир Фадин <vfadin@24hsoft.com>
2025-04-06 13:42:50 +0300

    frontend. Пробую исправить сборку next.js в ci

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import type { NextConfig } from 'next';
 
+process.chdir(__dirname);
+
 const nextConfig: NextConfig = {
 	experimental: {
 		optimizePackageImports: ['@chakra-ui/react'],
@@ -9,6 +11,9 @@ const nextConfig: NextConfig = {
 		tsconfigPath: './tsconfig.json',
 	},
 	webpack(config) {
+		// Явно задаём одну копию React и ReactDOM
+		config.resolve.alias['react'] = path.resolve(__dirname, 'node_modules', 'react');
+		config.resolve.alias['react-dom'] = path.resolve(__dirname, 'node_modules', 'react-dom');
 		config.resolve.alias['@'] = path.resolve(__dirname, 'src');
 		return config;
 	},
