44e9db88320565f0cf11aef667cb8cc7e1bab608
Владимир Фадин <vfadin@24hsoft.com>
2025-04-06 14:43:25 +0300

    Revert "frontend. Пробую исправить сборку next.js в ci"
    This reverts commit a5d2584e18bf3008117419c87efd78adee1267b0.

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,8 +1,6 @@
 import path from 'path';
 import type { NextConfig } from 'next';
 
-process.chdir(__dirname);
-
 const nextConfig: NextConfig = {
 	experimental: {
 		optimizePackageImports: ['@chakra-ui/react'],
@@ -11,9 +9,6 @@ const nextConfig: NextConfig = {
 		tsconfigPath: './tsconfig.json',
 	},
 	webpack(config) {
-		// Явно задаём одну копию React и ReactDOM
-		config.resolve.alias['react'] = path.resolve(__dirname, 'node_modules', 'react');
-		config.resolve.alias['react-dom'] = path.resolve(__dirname, 'node_modules', 'react-dom');
 		config.resolve.alias['@'] = path.resolve(__dirname, 'src');
 		return config;
 	},
