45cd341a07714dc89fdab5ad59cba9804d639a3b
Владимир Фадин <vfadin@24hsoft.com>
2025-04-08 21:32:59 +0300

    frontend. Добавил путь до api из Environment переменной

--- apps/frontend/src/lib/apollo-client.ts
+++ apps/frontend/src/lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 
 export const client = new ApolloClient({
-	uri: 'http://localhost:4000/graphql', // Замени на актуальный адрес
+	uri: process.env.NEXT_PUBLIC_API_URL,
 	cache: new InMemoryCache(),
-	credentials: 'include', // 🔥 ОБЯЗАТЕЛЬНО, иначе куки не передадутся!
+	credentials: 'include',
 });
