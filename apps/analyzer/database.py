0d26f66326984936e88b32257b25072a535b61b9
Владимир Фадин <vfadin@24hsoft.com>
2025-04-09 01:18:20 +0300

    analyzer. Попытка починить получение DATABASE_URL

--- apps/analyzer/database.py
+++ apps/analyzer/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = db_url = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:password@postgres:5433/portfolio_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:password@postgres:5433/portfolio_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
