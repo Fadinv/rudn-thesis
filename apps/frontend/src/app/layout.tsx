46536f191ec39d2bac56a7cf47161a6a63e9e065
Владимир Фадин <vfadin@24hsoft.com>
2025-04-26 22:18:33 +0300

    apps/frontend. Добавил meta user-scalable=no

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -5,6 +5,9 @@ import React from 'react';
 export default function RootLayout({children}: { children: React.ReactNode }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
+		<head>
+			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+		</head>
 		<body>
 		<ClientProviders>{children}</ClientProviders>
 		<Analytics/>
