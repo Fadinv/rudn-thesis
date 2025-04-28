030ab22d8d6bab61ddf0b4b81592f749d47b9b4b
Владимир Фадин <vfadin@24hsoft.com>
2025-04-28 23:33:02 +0300

    apps/backend. Исправил cors в app

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,13 +13,16 @@ async function bootstrap() {
 
 	// Настраиваем CORS
 	app.enableCors({
-		origin: [
-			'https://www.portfolioanalyzer.ru',
-			'https://portfolioanalyzer.ru',
-			'https://rudn-thesis.vercel.app',
-			'http://localhost:3000',
-			'http://localhost:4000',
-		],
+		origin: process.env.NODE_ENV === 'production'
+			? [
+				'https://www.portfolioanalyzer.ru',
+				'https://portfolioanalyzer.ru',
+				'https://rudn-thesis.vercel.app',
+			]
+			: [
+				'http://localhost:3000',
+				'http://localhost:4000',
+			],
 		credentials: true,
 	});
 
