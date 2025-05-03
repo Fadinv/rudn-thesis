a338d9fcf40ba72972ab607f992d3c11a7a2f6da
Владимир Фадин <vfadin@24hsoft.com>
2025-05-03 19:25:21 +0300

    apps/frontend. Фикс иконки favicon

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -2,21 +2,21 @@ import ClientProviders from '@frontend/app/clientProviders';
 import {Analytics} from '@vercel/analytics/next';
 import React from 'react';
 
-// export const metadata = {
-// 	icons: {
-// 		icon: '/favicon-v2.ico',
-// 		shortcut: '/favicon.ico',
-// 		apple: '/icons/icon-192.png',
-// 	},
-// 	manifest: '/manifest.json',
-// };
+export const metadata = {
+	icons: {
+		icon: '/favicon.ico',
+		shortcut: '/favicon.ico',
+		apple: '/icons/icon-192.png',
+	},
+	// manifest: '/manifest.json',
+};
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 		<head>
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
-			<link rel="icon" href="/favicon.ico" />
+			{/*<link rel="icon" href="/favicon.ico" />*/}
 		</head>
 		<body>
 		<ClientProviders>{children}</ClientProviders>
