cce0927f64d07479b317803165d448eaff18aa6d
Владимир Фадин <vfadin@24hsoft.com>
2025-05-04 18:22:19 +0300

    apps/frontend. Вернул метаданные, без индексации

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -3,21 +3,19 @@ import {Analytics} from '@vercel/analytics/next';
 import React from 'react';
 
 export const metadata = {
+	title: 'Portfolio Analyzer',
 	icons: {
 		icon: '/favicon.ico',
 		shortcut: '/favicon.ico',
 		apple: '/icons/icon-192.png',
 	},
+	viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
 	// manifest: '/manifest.json',
 };
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-		<head>
-			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
-			{/*<link rel="icon" href="/favicon.ico" />*/}
-		</head>
 		<body>
 		<ClientProviders>{children}</ClientProviders>
 		<Analytics/>
